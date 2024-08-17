9df4c81c1efacdce85e5ac4d6809521765f36a45
mstefani-source <ivanow.victor@live.ru>
2024-08-17 11:30:27 +0300

    add profiler to pom

--- src/main/java/za/vita/App.java
+++ src/main/java/za/vita/App.java
@@ -14,5 +14,11 @@ import org.springframework.context.annotation.EnableMBeanExport;
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+
+        // int [][] arr = {{1,5}, {4,2}};
+
+        // for (int i = 0; i < arr.length; i++) {
+        //     System.out.println(arr[i][i]);
+        // }
     }
 }
